9eaa8593054f5a8020a14e05af06d7e8576d1bce
KunoiSayami <i@leanhe.dev>
2022-08-11 03:11:52 +0800

    refactor: Migration to teloxide 0.10
    Signed-off-by: KunoiSayami <i@leanhe.dev>

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
 use anyhow::anyhow;
-use teloxide::{dispatching::DispatcherHandlerRx, prelude::*};
-use tokio_stream::wrappers::UnboundedReceiverStream;
+use teloxide::prelude::*;
 
-type MessageEvent = UpdateWithCx<AutoSend<Bot>, Message>;
+//type MessageEvent = UpdateWithCx<AutoSend<Bot>, Message>;
 type Result<T> = anyhow::Result<T>;
 
-async fn on_message(event: &MessageEvent) -> Result<()> {
-    let MessageEvent {
-        requester: bot,
-        update: message,
-    } = event;
-
+async fn handler(bot: AutoSend<Bot>, message: Message) -> Result<()> {
     let sender_chat = message
         .sender_chat()
         .ok_or_else(|| anyhow!("No sender chat"))?;
@@ -23,17 +17,20 @@ async fn on_message(event: &MessageEvent) -> Result<()> {
     let channel_id = bot.get_chat(message.chat.id).await?.linked_chat_id();
 
     // linked channel message
-    if channel_id == Some(sender_chat.id) {
+    if channel_id == Some(sender_chat.id.0) {
         return Ok(());
     }
 
-    if let Err(_) = event.delete_message().await {
+    if let Err(_) = bot.delete_message(message.chat.id, message.id).await {
         let _ = bot
             .send_message(
-                message.chat_id(),
+                message.chat.id,
                 "Detected a channel message that is not from the linked channel, but failed to delete it.\n\n\
                  Did you give me permission to delete messages?",
             )
+            .await;
+        let _ = bot
+            .ban_chat_sender_chat(message.chat.id, sender_chat.id)
             .await;
     }
 
@@ -43,14 +40,5 @@ async fn on_message(event: &MessageEvent) -> Result<()> {
 #[tokio::main]
 async fn main() {
     let bot = Bot::from_env().auto_send();
-
-    Dispatcher::new(bot)
-        .messages_handler(|event: DispatcherHandlerRx<AutoSend<Bot>, Message>| {
-            UnboundedReceiverStream::new(event).for_each_concurrent(None, |event| async move {
-                let _ = on_message(&event).await;
-                let _ = respond(());
-            })
-        })
-        .dispatch()
-        .await;
+    teloxide::repl(bot, handler).await
 }
